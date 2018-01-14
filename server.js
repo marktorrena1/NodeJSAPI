0ca33955007eb495f0db99fde346b2cc19f6212c
Mark Jeo Torrena <marktorrena1@gmail.com>
2018-01-14 15:32:26 +0800

    removed comments on server.js

--- server.js
+++ server.js
@@ -17,23 +17,6 @@ app.use(bodyParser.json())
 //api end points
 app.use('/api/auth', userCtrl);
 
-// app.get('/test',(req,res) => {
-//     res.json({result: 'Hello Mark Jeo'})
-// })
-
-// app.get('/token/:username',(req,res) => {
-//     res.json({result : req.params.username})
-// })
-
-// app.post('/token',(req,res) => {
-//     console.log(req.body)
-//     var data = {
-//         name : req.body.username,
-//         fullname: req.body.fullname
-//     }
-//     res.json(data)
-// })
-
 
 mongoose.connect('mongodb://markjeo:markjeo@ds251747.mlab.com:51747/nodedb',{ useMongoClient: true }, (err) => {
     if(!err) console.log('connected to mongo')
